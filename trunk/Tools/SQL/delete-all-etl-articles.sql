bcb266a5b21ee04e373dbb7906e8111b98f3dea1
harald.goettlicher@gmail.com <harald.goettlicher@gmail.com@76b99574-5675-6361-06a2-39f88614236d>
2012-07-08 21:29:33 +0000

    improved SQLs added rename

--- trunk/Tools/SQL/delete-all-etl-articles.sql
+++ trunk/Tools/SQL/delete-all-etl-articles.sql
@@ -1,8 +1,8 @@
 ﻿-- delete articles loaded by ETL
 -- DEFINITION: id is shorter than 30 -> no UUID -> loaded by ETL
-DELETE FROM STOCKCURRENT;
-DELETE FROM PRODUCTS WHERE length(PRODUCTS_CAT.product) < 36;
--- delete all non manually created catergories 
+-- delete all non manually created categories 
 DELETE FROM PRODUCTS_CAT WHERE length(PRODUCTS_CAT.product) < 36;
+-- delete all non manually created articles 
+DELETE FROM PRODUCTS WHERE length(PRODUCTS.id) < 36;
 -- delete combined product (deposit, hamper)
 DELETE FROM PRODUCTS_COM;
---
+++ trunk/Tools/SQL/rename-product-reference.sql
@@ -0,0 +1,7 @@
+﻿-- Example: rename product.reference starting with 'M' to strting with 'A'
+
+-- use this SQL to check first
+select * from products where substr(products.reference,1,1)='M'
+
+-- use this SQL to do the renaming
+update products set reference='A'||substr(reference,2) where substr(reference,1,1)='M'
