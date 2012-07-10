4493b37dbeaf1a93a35c50b5e7008ddc8d034b72
harald.goettlicher@gmail.com <harald.goettlicher@gmail.com@76b99574-5675-6361-06a2-39f88614236d>
2012-07-10 19:58:09 +0000

    added import function for resources

---
+++ trunk/OpenbravoPOS/src-pos/com/openbravo/pos/admin/ImportResources.java
@@ -0,0 +1,119 @@
+//    Openbravo POS is a point of sales application designed for touch screens.
+//    Copyright (C) 2007-2009 Openbravo, S.L.
+//    http://www.openbravo.com/product/pos
+//
+//    This file is part of Openbravo POS.
+//
+//    Openbravo POS is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Openbravo POS is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Openbravo POS.  If not, see <http://www.gnu.org/licenses/>.
+
+package com.openbravo.pos.admin;
+
+import com.openbravo.basic.BasicException;
+import com.openbravo.data.gui.JMessageDialog;
+import com.openbravo.data.gui.MessageInf;
+import com.openbravo.data.loader.BatchSentence;
+import com.openbravo.data.loader.BatchSentenceResource;
+import com.openbravo.pos.forms.AppLocal;
+import com.openbravo.pos.forms.AppView;
+import com.openbravo.pos.forms.DataLogicSystem;
+import com.openbravo.pos.forms.JPanelView;
+import com.openbravo.pos.forms.JRootApp;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Import resources from disk to DB, overwrite all.<br>
+ * Useful for using an external editor.
+ * @author Harald
+ */
+public class ImportResources extends JPanel implements JPanelView {
+
+    private AppView m_App;
+    private String m_lastStatus;
+    private JTextField m_jText;
+
+    public ImportResources (AppView oApp) {
+        m_App = oApp;
+        m_jText = new JTextField(m_lastStatus);
+        add (m_jText);
+    }
+    
+    /**
+     * Run the import itself, script taken from src-pos/com/openbravo/pos/scripts/<DB-type>-importResources.sql.
+     * @param app
+     * @return true - imported correctly
+     */
+    private boolean runImport (AppView app) {
+
+        DataLogicSystem dlSystem = (DataLogicSystem) app.getBean("com.openbravo.pos.forms.DataLogicSystem");
+        m_lastStatus = "Running...";
+        
+        String sScript = dlSystem.getInitScript() + "-importResources.sql";
+
+        if (JRootApp.class.getResource(sScript) == null) {
+            JMessageDialog.showMessage(this, new MessageInf(MessageInf.SGN_DANGER, AppLocal.getIntString("message.noImportResourcesScript", app.getSession().DB.getName())));
+            return false;
+        } else {
+            // import script exists
+            if (JOptionPane.showConfirmDialog(this
+                    , AppLocal.getIntString("message.ImportResources") // Really import resources?
+                    , AppLocal.getIntString("message.title")
+                    , JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {  
+
+                try {
+                    BatchSentence bsentence = new BatchSentenceResource(app.getSession(), sScript);
+
+                    // method list() is executing the SQL query
+                    java.util.List l = bsentence.list();
+                    if (l.size() > 0) {
+                        JMessageDialog.showMessage(this, new MessageInf(MessageInf.SGN_WARNING, AppLocal.getIntString("Database.ScriptWarning"), l.toArray(new Throwable[l.size()])));
+                        m_lastStatus = AppLocal.getIntString("Database.ScriptWarning") + "\n";
+                    }
+               } catch (BasicException e) {
+                    JMessageDialog.showMessage(this, new MessageInf(MessageInf.SGN_DANGER, AppLocal.getIntString("Database.ScriptError"), e));
+                    return false;
+                }     
+            } else {
+                return false;
+            }
+        }   
+
+        m_lastStatus += AppLocal.getIntString("message.ImportResourcesDone");
+        return true;
+
+    }
+
+    @Override
+    public String getTitle() {
+        return AppLocal.getIntString("Menu.ImportResources");
+    }
+
+    @Override
+    public void activate() throws BasicException {
+        runImport(m_App);
+    }
+
+    @Override
+    public boolean deactivate() {
+        return true;
+    }
+
+    @Override
+    public JComponent getComponent() {
+        return m_jText;
+    }
+    
+}
---
+++ trunk/OpenbravoPOS/src-pos/com/openbravo/pos/scripts/PostgreSQL-importResources.sql
@@ -0,0 +1,56 @@
+--    Openbravo POS is a point of sales application designed for touch screens.
+--    Copyright (C) 2007-2010 Openbravo, S.L.
+--    http://sourceforge.net/projects/openbravopos
+--
+--    This file is part of Openbravo POS.
+--
+--    Openbravo POS is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Openbravo POS is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Openbravo POS.  If not, see <http://www.gnu.org/licenses/>.
+
+-- Import resources script for POSTGRESQL
+
+DELETE FROM RESOURCES;
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('0', 'Printer.Start', 0, $FILE{/com/openbravo/pos/resources/Printer.Start.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('1', 'Printer.Ticket', 0, $FILE{/com/openbravo/pos/resources/Printer.Ticket.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('2', 'Printer.Ticket2', 0, $FILE{/com/openbravo/pos/resources/Printer.Ticket2.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('3', 'Printer.TicketPreview', 0, $FILE{/com/openbravo/pos/resources/Printer.TicketPreview.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('4', 'Printer.TicketTotal', 0, $FILE{/com/openbravo/pos/resources/Printer.TicketTotal.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('5', 'Printer.OpenDrawer', 0, $FILE{/com/openbravo/pos/resources/Printer.OpenDrawer.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('6', 'Printer.Ticket.Logo', 1, $FILE{/com/openbravo/pos/resources/Printer.Ticket.Logo.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('7', 'Printer.TicketLine', 0, $FILE{/com/openbravo/pos/resources/Printer.TicketLine.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('8', 'Printer.CloseCash', 0, $FILE{/com/openbravo/pos/resources/Printer.CloseCash.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('9', 'Window.Logo', 1, $FILE{/com/openbravo/pos/resources/Window.Logo.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('10', 'Window.Title', 0, $FILE{/com/openbravo/pos/resources/Window.Title.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('11', 'Ticket.Buttons', 0, $FILE{/com/openbravo/pos/resources/Ticket.Buttons.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('12', 'Ticket.Line', 0, $FILE{/com/openbravo/pos/resources/Ticket.Line.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('13', 'Printer.Inventory', 0, $FILE{/com/openbravo/pos/resources/Printer.Inventory.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('14', 'Menu.Root', 0, $FILE{/com/openbravo/pos/resources/Menu.Root.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('15', 'Printer.CustomerPaid', 0, $FILE{/com/openbravo/pos/resources/Printer.CustomerPaid.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('16', 'Printer.CustomerPaid2', 0, $FILE{/com/openbravo/pos/resources/Printer.CustomerPaid2.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('17', 'payment.cash', 0, $FILE{/com/openbravo/pos/resources/payment.cash.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('18', 'banknote.50euro', 1, $FILE{/com/openbravo/pos/resources/banknote.50euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('19', 'banknote.20euro', 1, $FILE{/com/openbravo/pos/resources/banknote.20euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('20', 'banknote.10euro', 1, $FILE{/com/openbravo/pos/resources/banknote.10euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('21', 'banknote.5euro', 1, $FILE{/com/openbravo/pos/resources/banknote.5euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('22', 'coin.2euro', 1, $FILE{/com/openbravo/pos/resources/coin.2euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('23', 'coin.1euro', 1, $FILE{/com/openbravo/pos/resources/coin.1euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('24', 'coin.50cent', 1, $FILE{/com/openbravo/pos/resources/coin.50cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('25', 'coin.20cent', 1, $FILE{/com/openbravo/pos/resources/coin.20cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('26', 'coin.10cent', 1, $FILE{/com/openbravo/pos/resources/coin.10cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('27', 'coin.5cent', 1, $FILE{/com/openbravo/pos/resources/coin.5cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('28', 'coin.2cent', 1, $FILE{/com/openbravo/pos/resources/coin.2cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('29', 'coin.1cent', 1, $FILE{/com/openbravo/pos/resources/coin.1cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('30', 'Printer.PartialCash', 0, $FILE{/com/openbravo/pos/resources/Printer.PartialCash.xml});
+-- custom fairpos ressources
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('31', 'event.close', 0, $FILE{/com/openbravo/pos/resources/event.close.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('32', 'payments.templates', 0, $FILE{/com/openbravo/pos/resources/payments.templates.txt});
