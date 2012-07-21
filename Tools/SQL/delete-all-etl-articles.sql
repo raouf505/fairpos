f253d532e32809bc849900b682615ef381f2b125
harald.goettlicher <harald.goettlicher@gmail.com>
2012-07-21 21:48:54 +0000

    order was not correct

--- Tools/SQL/delete-all-etl-articles.sql
+++ Tools/SQL/delete-all-etl-articles.sql
@@ -1,8 +1,8 @@
 ﻿-- delete articles loaded by ETL
 -- DEFINITION: id is shorter than 30 -> no UUID -> loaded by ETL
+-- delete combined product (deposit, hamper)
+DELETE FROM PRODUCTS_COM;
 -- delete all non manually created categories 
 DELETE FROM PRODUCTS_CAT WHERE length(PRODUCTS_CAT.product) < 36;
 -- delete all non manually created articles 
 DELETE FROM PRODUCTS WHERE length(PRODUCTS.id) < 36;
--- delete combined product (deposit, hamper)
-DELETE FROM PRODUCTS_COM;
